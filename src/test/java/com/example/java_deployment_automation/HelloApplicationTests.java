5a9bda55382205238d1edaa29e141d3cd46edd2c
Ebbe Wertz <146071304+EbbeWertzStudentUH@users.noreply.github.com>
2024-11-17 00:04:46 +0100

    test test

---
+++ src/test/java/com/example/java_deployment_automation/HelloApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example.java_deployment_automation;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloApplicationTests {
+
+    @Test
+    public void testTest() {assert true;}
+}
